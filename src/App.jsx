0ded33387dd359a7ab32ce066d3f4eb4f861f2d6
charoulafotiadou <123567671+charoulafotiadou@users.noreply.github.com>
2024-01-26 10:12:14 +0100

    Possible corrections in running

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Signup from "./components/Signup"
 import Login from "./components/Login"
 import Home from "./components/Home"
 
 const App = () => {
-  return (
-    <div className="w-11/12 flex flex-col mx-auto">
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/signup" element={<Signup />} />
-    <Route path="/login" element={<Login />} />
-    </Routes>
+  const myDiv = (
+    <div>
+      <h1>Hello</h1>
     </div>
+  );
+
+
+  return (
+    <BrowserRouter>
+      <div className="w-11/12 flex flex-col mx-auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {myDiv}
+    </div>
+    </BrowserRouter>
   )
-}
+};
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -10,3 +10,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Toaster />
   </BrowserRouter>
 );
+
